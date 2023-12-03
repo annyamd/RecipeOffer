f2e296cb6c112d9fa3f4b0f38038e21a571e4d41
annyamd <miann9920@gmail.com>
2023-12-03 18:33:25 +0300

    no lateinit for views anymore

--- app/src/main/java/com/example/recipeoffer/EnterFragment.kt
+++ app/src/main/java/com/example/recipeoffer/EnterFragment.kt
@@ -1,42 +1,50 @@
 package com.example.recipeoffer
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.room.Room
+import com.example.recipeoffer.databinding.FragmentEnterBinding
 import com.example.recipeoffer.db.AppDatabase
 
 class EnterFragment : Fragment(R.layout.fragment_enter) {
 
     private var appDatabase: AppDatabase? = null
-    private lateinit var loginEditView: EditText
-    private lateinit var passwordEditView: EditText
-    private lateinit var loginBtn: Button
+    private var _binding: FragmentEnterBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEnterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         if (savedInstanceState == null) {
             createDatabase()
-            bindViews(view)
 
 //            appDatabase?.getUserDao()?.addUser(User("anya", "123"))
 
-            view.findViewById<Button>(R.id.login_button)
-                .setOnClickListener {
-                    val login = loginEditView.text.toString()
-                    val password = passwordEditView.text.toString()
+            binding.loginButton.setOnClickListener {
+                val login = binding.loginEt.text.toString()
+                val password = binding.passwordEt.text.toString()
 
-                    val foundUser = appDatabase?.getUserDao()?.getUserByLogin(login)
-                    if (foundUser?.password == password) {
-                        Toast.makeText(context, "YOU PASSED", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(context, "NO PASS... SORRY", Toast.LENGTH_SHORT).show()
-                    }
+                val foundUser = appDatabase?.getUserDao()?.getUserByLogin(login)
+                if (foundUser?.password == password) {
+                    Toast.makeText(context, "YOU PASSED", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "NO PASS... SORRY", Toast.LENGTH_SHORT).show()
                 }
+            }
         }
     }
 
@@ -55,10 +63,9 @@ class EnterFragment : Fragment(R.layout.fragment_enter) {
 
     }
 
-    private fun bindViews(view: View) {
-        loginEditView = view.findViewById(R.id.login_et)
-        loginBtn = view.findViewById(R.id.login_button)
-        passwordEditView = view.findViewById(R.id.password_et)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
